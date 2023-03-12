e0e6a2bdea6af10bb78f5404cfb324a7df082197
Prasanna Rajendran <imprasannarajendran@gmail.com>
2023-03-13 00:30:57 +0530

    Fast Payments, Reports & Charts Added

---
+++ main.js
@@ -0,0 +1,47 @@
+const chart = document.querySelector("#chart").getContext("2d");
+
+new Chart(chart, {
+  type: "line",
+  data: {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Noc",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "BTC",
+        data: [
+          29374, 33537, 49631, 59095, 36684, 33572, 39000, 48847, 48116, 61004,
+        ],
+        borderColor: "red",
+        borderWidth: 2,
+      },
+      {
+        label: "ETH",
+        data: [
+          15374, 20537, 21631, 29095, 20000, 20572, 19005, 38847, 38116, 40004,
+        ],
+        borderColor: "blue",
+        borderWidth: 2,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
